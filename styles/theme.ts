2b04ab6610291dbe58a5c246b8904f3ca24995e1
arthurolmos <arthur.olmos@gmail.com>
2022-02-07 10:14:22 -0300

    Updated Styles, added ThemeContext to _app

---
+++ styles/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  main: {
+    orange: "#c86420",
+    darkOrange: "#975310",
+    white: "#ffffff",
+    gray: "#6f6f6f",
+    darkgray: "#2f2f2f",
+  },
+  inverted: {
+    background: "#c86420",
+    color: "#ffffff",
+  },
+};
