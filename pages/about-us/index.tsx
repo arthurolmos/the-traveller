9d3cf62124dba70f09e39be05e53d24b0831921b
arthurolmos <arthur.olmos@gmail.com>
2022-02-07 16:05:14 -0300

    Started About Us page

---
+++ pages/about-us/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import MainContainer from "../../components/layout/MainContainer";
+import PageComponent from "../../components/layout/PageComponent";
+
+export default function AboutUs() {
+  return (
+    <MainContainer title="About Us">
+      <PageComponent title="About Us">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam,
+          impedit quo. Harum atque reprehenderit aliquam odio, illo ullam
+          provident numquam.
+        </p>
+      </PageComponent>
+    </MainContainer>
+  );
+}
