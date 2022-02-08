127fa4841edd18ee7ff424c2b0c19210cfbe3e8f
Arthur Olmos <arthur.olmos@gmail.com>
2022-02-08 16:42:37 -0300

    Finished SignIn and SignUp pages

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -1,5 +1,88 @@
-import React from "react";
+import React from 'react';
+import MainContainer from '../../components/layout/MainContainer';
+import PageComponent from '../../components/layout/PageComponent';
+import DefaultInput from '../../components/input/DefaultInput';
+import DefaultButton from '../../components/buttons/DefaultButton';
+import { toast } from 'react-toastify';
+import exceptionHandler from '../../lib/exceptionHandler';
+import { useRouter } from 'next/router';
+import {
+  AuthAction,
+  withAuthUser,
+  withAuthUserTokenSSR,
+} from 'next-firebase-auth';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import {
+  SignInFormStyled,
+  SpinnerContainerStyled,
+} from '../../styles/pages/SignIn';
+import Link from 'next/link';
+import { BeatLoaderSpinner } from '../../components/spinners/BeatLoader';
 
-export default function SignIn() {
-  return <div></div>;
+export function SignIn() {
+  const auth = getAuth();
+  const router = useRouter();
+
+  const [loading, setLoading] = React.useState(false);
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const submit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      if ([email, password].every((item) => item !== '')) {
+        setLoading(true);
+        await signInWithEmailAndPassword(auth, email, password);
+
+        setLoading(false);
+        router.push('/');
+      } else {
+        throw new Error('Fill all fields!');
+      }
+    } catch (err) {
+      setLoading(false);
+
+      const message = exceptionHandler(err);
+      toast.warn(message);
+    }
+  };
+
+  return (
+    <MainContainer title="Sign In">
+      <PageComponent title="Sign In">
+        <SignInFormStyled>
+          <DefaultInput
+            placeholder="Email"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <DefaultInput
+            type="new-password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          {loading ? (
+            <SpinnerContainerStyled>
+              <BeatLoaderSpinner loading={loading} />
+            </SpinnerContainerStyled>
+          ) : (
+            <DefaultButton title="Create Account" inverted onClick={submit} />
+          )}
+        </SignInFormStyled>
+
+        <p style={{ textAlign: 'center' }}>
+          Don`t have an account? <Link href="/signin">Click here!</Link>
+        </p>
+      </PageComponent>
+    </MainContainer>
+  );
 }
+
+export const getServerSideProps = withAuthUserTokenSSR({
+  whenAuthed: AuthAction.REDIRECT_TO_APP,
+})();
+
+export default withAuthUser()(SignIn);
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,9 +1,143 @@
-import React from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React from 'react';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth';
+import MainContainer from '../../components/layout/MainContainer';
+import PageComponent from '../../components/layout/PageComponent';
+import DefaultInput from '../../components/input/DefaultInput';
+import DefaultButton from '../../components/buttons/DefaultButton';
+import { toast } from 'react-toastify';
+import exceptionHandler from '../../lib/exceptionHandler';
+import { useRouter } from 'next/router';
+import {
+  AuthAction,
+  withAuthUser,
+  withAuthUserTokenSSR,
+} from 'next-firebase-auth';
+import {
+  SignUpFormStyled,
+  SpinnerContainerStyled,
+} from '../../styles/pages/SignUp';
+import Link from 'next/link';
+import { BeatLoaderSpinner } from '../../components/spinners/BeatLoader';
 
-export default function SignUp() {
+export function SignUp() {
   const auth = getAuth();
+  const router = useRouter();
 
-  console.log({ auth });
-  return <div></div>;
+  const [loading, setLoading] = React.useState(false);
+  const [firstName, setFirstName] = React.useState('');
+  const [lastName, setLastName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [confirmPassword, setConfirmPassword] = React.useState('');
+
+  const submit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      if (
+        [firstName, lastName, email, password, confirmPassword].every(
+          (item) => item !== ''
+        )
+      ) {
+        if (password !== confirmPassword)
+          throw new Error('Passwords not matching!');
+
+        setLoading(true);
+
+        const { user } = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        await updateProfile(user, {
+          displayName: `${firstName} ${lastName}`,
+        });
+
+        setLoading(false);
+        router.push('/');
+      } else {
+        throw new Error('Fill all fields!');
+      }
+    } catch (err) {
+      console.error({ err });
+
+      setLoading(false);
+
+      const message = exceptionHandler(err);
+      toast.warn(message);
+    }
+  };
+
+  return (
+    <MainContainer title="Sign Up">
+      <PageComponent title="Sign Up">
+        <SignUpFormStyled
+          style={{
+            gap: 20,
+            display: 'flex',
+            flexDirection: 'column',
+            marginBottom: 30,
+            width: 500,
+          }}
+          onSubmit={submit}
+        >
+          <DefaultInput
+            placeholder="First Name"
+            type="text"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+
+          <DefaultInput
+            placeholder="Last Name"
+            type="text"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+
+          <DefaultInput
+            placeholder="Email"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <DefaultInput
+            type="new-password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <DefaultInput
+            style={{
+              borderColor: password === confirmPassword ? 'lightgreen' : 'red',
+            }}
+            type="new-password"
+            placeholder="Confirm Password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+
+          {loading ? (
+            <SpinnerContainerStyled>
+              <BeatLoaderSpinner loading={loading} />
+            </SpinnerContainerStyled>
+          ) : (
+            <DefaultButton title="Create Account" inverted onClick={submit} />
+          )}
+        </SignUpFormStyled>
+
+        <p style={{ textAlign: 'center' }}>
+          Already have an account? <Link href="/signin">Click here!</Link>
+        </p>
+      </PageComponent>
+    </MainContainer>
+  );
 }
+
+export const getServerSideProps = withAuthUserTokenSSR({
+  whenAuthed: AuthAction.REDIRECT_TO_APP,
+})();
+
+export default withAuthUser()(SignUp);
