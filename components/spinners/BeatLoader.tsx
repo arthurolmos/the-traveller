6d1d72e563564031f66fb9ffd11e55408cd7c67e
Arthur Olmos <arthur.olmos@gmail.com>
2022-02-08 16:42:05 -0300

    Created Spinner component

---
+++ components/spinners/BeatLoader.tsx
@@ -0,0 +1,16 @@
+import BeatLoader from 'react-spinners/BeatLoader';
+
+interface Props {
+  loading: boolean;
+}
+
+export function BeatLoaderSpinner({ loading = false }: Props) {
+  return (
+    <BeatLoader
+      color="#c86420"
+      loading={loading}
+      size={30}
+      speedMultiplier={0.5}
+    />
+  );
+}
