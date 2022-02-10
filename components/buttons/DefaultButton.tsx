95ea3b9eeeed50ff91eed98d2bcf75604817d7d6
arthurolmos <arthur.olmos@gmail.com>
2022-02-10 11:04:02 -0300

    Updated DefaultButton

--- components/buttons/DefaultButton.tsx
+++ components/buttons/DefaultButton.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { DefaultButtonStyled } from '../../styles/components/buttons';
 
-interface Props {
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
   inverted?: boolean;
   onClick: (e: React.FormEvent) => void;
 }
 
 export default function DefaultButton(props: Props) {
-  const { title, inverted = false, onClick } = props;
+  const { title, inverted = false, onClick, ...rest } = props;
 
   return (
     <DefaultButtonStyled inverted={inverted} onClick={onClick}>
