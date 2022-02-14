8e851baf79408c51ce17db2a7daa0ae020b50c67
arthurolmos <arthur.olmos@gmail.com>
2022-02-14 10:13:01 -0300

    Moved animations to separate folder

--- styles/pages/Locations.tsx
+++ styles/pages/Locations.tsx
@@ -1,13 +1,5 @@
-import styled, { keyframes } from 'styled-components';
-
-const fadeIn = keyframes`
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
-`;
+import styled from 'styled-components';
+import { fadeIn } from '../components/animations';
 
 export const ListStyled = styled.ul`
   padding: 0;
