fe353bb56e28caa36e30e477f98d33fa349b00b1
arthurolmos <arthur.olmos@gmail.com>
2022-02-14 17:00:57 -0300

    Added more fields for Author

--- interfaces/IPost.tsx
+++ interfaces/IPost.tsx
@@ -1,3 +1,5 @@
+import { Timestamp } from 'firebase/firestore';
+
 export enum IPostStatus {
   PENDING_APPROVAL = 'Pending Approval',
   REJECTED = 'Rejected',
@@ -8,7 +10,11 @@ export interface IPost {
   id?: string;
   title: string;
   text: string;
-  author: string;
+  authorId: string;
+  authorName: string;
   status: IPostStatus;
   coverImage?: string;
+  createdAt: Date | Timestamp;
+  updatedAt: Date | Timestamp;
+  approvedAt?: Date | Timestamp;
 }
