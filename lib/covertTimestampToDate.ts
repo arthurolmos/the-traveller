ec9d19674699250bfef6be13182f0f30e8b03224
arthurolmos <arthur.olmos@gmail.com>
2022-02-16 12:27:49 -0300

    Removed console log

--- lib/covertTimestampToDate.ts
+++ lib/covertTimestampToDate.ts
@@ -3,7 +3,5 @@ import { Timestamp } from 'firebase/firestore';
 export default function convertTimestampToDate(timestamp: Timestamp) {
   const date = timestamp.toDate().toLocaleString();
 
-  console.log({ date });
-
   return date;
 }
