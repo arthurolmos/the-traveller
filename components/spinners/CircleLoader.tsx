9ced5379768fc063799a20e5c15043c29ff48b3a
arthurolmos <arthur.olmos@gmail.com>
2022-02-18 11:37:03 -0300

    Updating Spinners

--- components/spinners/CircleLoader.tsx
+++ components/spinners/CircleLoader.tsx
@@ -2,14 +2,15 @@ import CircleLoader from 'react-spinners/CircleLoader';
 
 interface Props {
   loading: boolean;
+  size?: number;
 }
 
-export function CircleLoaderSpinner({ loading = false }: Props) {
+export function CircleLoaderSpinner({ loading = false, size = 80 }: Props) {
   return (
     <CircleLoader
       color="#c86420"
       loading={loading}
-      size={80}
+      size={size}
       // speedMultiplier={0.5}
     />
   );
--- components/spinners/ClipLoader.tsx
+++ components/spinners/ClipLoader.tsx
@@ -2,14 +2,15 @@ import ClipLoader from 'react-spinners/ClipLoader';
 
 interface Props {
   loading: boolean;
+  size?: number;
 }
 
-export function ClipLoaderSpinner({ loading = false }: Props) {
+export function ClipLoaderSpinner({ loading = false, size = 80 }: Props) {
   return (
     <ClipLoader
       color="#c86420"
       loading={loading}
-      size={80}
+      size={size}
       speedMultiplier={0.3}
     />
   );
