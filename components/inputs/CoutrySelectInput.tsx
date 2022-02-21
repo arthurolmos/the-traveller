e72950bdacea60ee1e3d00adee58db5abea136a5
arthurolmos <arthur.olmos@gmail.com>
2022-02-21 12:15:17 -0300

    Created CountrySelect input

--- components/inputs/CoutrySelectInput.tsx
+++ components/inputs/CoutrySelectInput.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import countryList from 'react-select-country-list';
 import { SelectInput } from '.';
+import { ICountry } from '../../interfaces/ICountry';
 
 interface Props {
-  value: string;
-  onChange: React.Dispatch<React.SetStateAction<string>>;
+  value: ICountry;
+  onChange: React.Dispatch<React.SetStateAction<ICountry>>;
 }
 
 export function CountrySelectInput({ value, onChange }: Props) {
   const options = React.useMemo(() => countryList().getData(), []);
 
-  return <SelectInput options={options} value={value} onChange={onChange} />;
+  return (
+    <SelectInput
+      options={options}
+      value={value}
+      onChange={onChange}
+      id="long-value-select"
+      instanceId="long-value-select"
+    />
+  );
 }
